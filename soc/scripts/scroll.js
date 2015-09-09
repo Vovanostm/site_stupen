67b0a3b9fd87c6f4f40846791b1b362808f74e87
Владимир Шеламов <vovanostm@MacBook-Pro-Vladimir.local>
2015-09-09 22:00:01 +0300

    mnogo chego

--- soc/scripts/scroll.js
+++ soc/scripts/scroll.js
@@ -1,19 +1,18 @@
+
+
 var x=0;
 jQuery(function( $ ){
     /**
      * Demo binding and preparation, no need to read this part
      */
         //borrowed from jQuery easing plugin
-        //http://gsgd.co.uk/sandbox/jquery.easing.php
-
-    
+        //http://gsgd.co.uk/sandbox/jquery.easing.php 
     // Settings examples bindings, they will all scroll to the same place, with different settings
     var $settings = $('#pane-settings');
     $('#settings-examples a').click(function() {
         // before each animation, reset to (0,0), skip this.
         
-    });
-    
+    });    
     $('#settings-axis').click(function() {// only scroll horizontally
         x=x+1;
         $settings.scrollTo('li:eq('+x+')', 1000, { axis:'x' });
