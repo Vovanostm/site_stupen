1a065bf4124c6314a2d010eef474f8eac3e5d384
Vovanostm <axb48@yandex.ru>
2016-05-17 11:16:33 +0300

    add mnogo chego

--- pro/scripts/o_prj.js
+++ pro/scripts/o_prj.js
@@ -1,18 +1,30 @@
 
 "use strict";
 
+var iframe = prj_frame;
 function show_project(event)
 {
 	event.preventDefault();
-	prj_frame.src=this.href;
-	prj_div.classList.remove("hid");
-	all_prj.classList.add("hid");
+	iframe.src=this.href;
+	console.log(iframe);
+	iframe.onload = function(){
+		prj_div.classList.remove("hid");
+		all_prj.classList.add("hid");
+		document.getElementsByTagName("body")[0].style.overflow="hidden";
+		var imgs=iframe.contentWindow.document.getElementsByTagName("img");
+		for (var i = 0; i< imgs.length; i++){
+			imgs[i].addEventListener("click",function(){prj_close.style.display="none";});
+		}
+		var cls_gal=iframe.contentWindow.document.getElementById('cls_gallery');
+		cls_gal.addEventListener("click",function(){prj_close.style.display="block"});
+	}
 }
 
 function close_project()
 {
 	prj_div.classList.add("hid");
 	all_prj.classList.remove("hid");
+	document.getElementsByTagName("body")[0].style.overflow="auto";
 }
 
 prj_close.addEventListener("click",close_project);
