fc2dee677d054f772a309f18889762ae4a97508d
Vovanostm <axb48@yandex.ru>
2016-10-10 22:06:59 +0300

    attentions

--- pro/scripts/attens.js
+++ pro/scripts/attens.js
@@ -2,14 +2,17 @@ $("li.a_ns")
 	.click(function(event) {
 		/* Act on the event */
 
-		//$(this).animate({opacity: 0}, 500 );
+		// $(this).animate({opacity: 0}, 500 );
 		// alert($(this).children('span').width());
-		// $("hr.a_ns").animate({left:$(this).children('span').position().left+"px", width:$(this).children('span').width()},600);
+		$("hr.a_ns").animate({left:$(this).children('span').position().left+"px", width:$(this).children('span').width()},600);
 
 		/* Act on the event */
 	});
 		/* Stuff to do when the mouse enters the element */
-// $("hr.a_ns").css({left:$("li.a_ns").children('span').position().left+"px"});
+
+$(document).ready(function(event){
+	$("hr.a_ns").animate({left:$("li.a_ns").children('span').position().left+"px", width:$("li.a_ns").children('span').width()},600);
+});
 
 function scrollAt(attns)
 {
